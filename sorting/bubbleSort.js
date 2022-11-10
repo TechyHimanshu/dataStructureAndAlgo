2749ed984df69997f3c053c5f632261d638d3b5e
Himanshu Jain <himanshuj@azuga.com>
2022-11-10 23:32:50 +0530

    FE practice

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -10,7 +10,6 @@ const bubbleSort = arr =>{
     for(let i = 0 ; i < arr.length; i++ ){
         for(let j = 0; j < arr.length-i-1; j++){
             if (arr[j+1]<arr[j]){
-            count +=1;
             [arr[j+1],arr[j]] = [arr[j],arr[j+1]]
             }
             console.log(arr);
